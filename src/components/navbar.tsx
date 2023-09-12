11026551bf9d7ff5a8b895dc115f384c40159d4f
Rohit Tokala <rtokala@purdue.edu>
2023-09-12 10:57:10 -0400

    Add kinde auth

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,17 @@
-import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./ui/mode-toggle";
 import DrawerToggle from "./drawer-toggle";
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export default function Navbar() {
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const user = getUser();
   return (
     <nav className=" w-screen fixed top-0 h-12 pr-2 flex items-center justify-between">
       <div className="flex gap-2 items-center">
@@ -14,6 +22,23 @@ export default function Navbar() {
       </div>
       <div className=" flex items-center gap-2">
         <ModeToggle />
+        {isAuthenticated() ? (
+          <>
+            <Button asChild>
+              <LogoutLink>Sign Out</LogoutLink>
+            </Button>
+            <Avatar>
+              <AvatarImage src={user.picture!} />
+              <AvatarFallback>{`${user.given_name?.charAt(
+                0
+              )}${user.family_name?.charAt(0)}`}</AvatarFallback>
+            </Avatar>
+          </>
+        ) : (
+          <Button asChild>
+            <LoginLink>Sign In</LoginLink>
+          </Button>
+        )}
       </div>
     </nav>
   );
